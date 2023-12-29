7c19e17a04c4d810a76b0964974f72ac01753fca
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-12-29 23:01:31 +0100

    Add styling for the auth login page

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,15 +1,25 @@
 import { githubSignIn, googleSignIn } from "@/actions/auth";
+import { BsGithub } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
+import css from "./page.module.css";
 
 export default function LoginPage() {
     return (
-        <div>
-            <h1>zen</h1>
-            <form action={githubSignIn}>
-                <button>Sign In With Github</button>
-            </form>
-            <form action={googleSignIn}>
-                <button>Sign In With Google</button>
-            </form>
+        <div className={css.wrapper}>
+            <div className={css.container}>
+                <h1 className={css.heading}>zen</h1>
+                <form action={githubSignIn}>
+                    <button className={css.github}>
+                        <BsGithub className={css.icon} /> Continue With Github
+                    </button>
+                </form>
+                <form action={googleSignIn}>
+                    <button className={css.google}>
+                        <FcGoogle className={css.icon} />
+                        Continue With Google
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
