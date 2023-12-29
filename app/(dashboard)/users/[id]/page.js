b09c313aeaa6c5c4772cf5c0657c903cea8ebbbc
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-12-29 19:02:15 +0100

    Show message when user has no rooms

--- app/(dashboard)/users/[id]/page.js
+++ app/(dashboard)/users/[id]/page.js
@@ -12,21 +12,27 @@ export default async function UserPage({ params: { id } }) {
                 <UserBox user={userInfo} />
             </div>
             <h2 className={css.heading}>User Rooms</h2>
-            <div className={css["room-list-wrapper"]}>
-                <div className={css["room-list"]}>
-                    {userInfo.adminRooms.map((room) => (
-                        <Link key={room.id} className={css["room-link"]} href={`/rooms/${room.id}`}>
-                            <div className={css["room-link-name"]}>{room.name}</div>
-                            <div className={css["room-link-admin"]}>Admin</div>
-                        </Link>
-                    ))}
-                    {userInfo.memberRooms.map((room) => (
-                        <Link key={room.id} className={css["room-link"]} href={`/rooms/${room.id}`}>
-                            <div className={css["room-link-name"]}>{room.name}</div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
+            {userInfo.adminRooms.length > 0 || userInfo.memberRooms.length > 0 ? (
+                <>
+                    <div className={css["room-list-wrapper"]}>
+                        <div className={css["room-list"]}>
+                            {userInfo.adminRooms.map((room) => (
+                                <Link key={room.id} className={css["room-link"]} href={`/rooms/${room.id}`}>
+                                    <div className={css["room-link-name"]}>{room.name}</div>
+                                    <div className={css["room-link-admin"]}>Admin</div>
+                                </Link>
+                            ))}
+                            {userInfo.memberRooms.map((room) => (
+                                <Link key={room.id} className={css["room-link"]} href={`/rooms/${room.id}`}>
+                                    <div className={css["room-link-name"]}>{room.name}</div>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div className={css["no-rooms"]}>This user is not in any rooms.</div>
+            )}
         </div>
     );
 }
