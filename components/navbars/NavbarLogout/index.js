4af2ca4e9af4452ba67de8d5bd29c2170a3bd8c0
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-12-30 16:11:47 +0100

    Change NavbarLogout button to SubmitButton

--- components/navbars/NavbarLogout/index.js
+++ components/navbars/NavbarLogout/index.js
@@ -1,11 +1,12 @@
 import { signOut } from "@/actions/auth";
+import SubmitButton from "@/components/general/SubmitButton";
 import cssNavbar from "../index.module.css";
 
 export default function NavbarLogout() {
     return (
         <form action={signOut} className={cssNavbar.container}>
             <div className={cssNavbar["main-buttons"]}>
-                <button className={cssNavbar["main-button"]}>Logout</button>
+                <SubmitButton className={cssNavbar["main-button"]} content="Logout" loading="Logging out..." />
             </div>
         </form>
     );
