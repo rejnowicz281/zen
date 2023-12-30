7222d1eda139e5f4f4c12e0a3e3c07e54f7f6048
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-12-30 17:40:38 +0100

    Update the error page

--- app/error.js
+++ app/error.js
@@ -1,11 +1,28 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import css from "./error.module.css";
 
 export default function ErrorPage() {
+    const pathname = usePathname();
+
     return (
-        <div>
-            There has been an error. Have you tried <Link href="/login">Logging in</Link>?
+        <div className={css.container}>
+            <p>An error has occured while loading the page. 🤔</p>
+            <p>You might wanna try...</p>
+            <ul>
+                <li>
+                    <a href={pathname}>Refreshing</a> the page
+                </li>
+                <li>Making sure the URL is correct</li>
+                <li>
+                    <Link href="/login">Logging in</Link>
+                </li>
+                <li>
+                    <Link href="/">Going back to the homepage</Link>
+                </li>
+            </ul>
         </div>
     );
 }
