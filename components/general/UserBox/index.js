fd873e4af7813d70ddf5ec921c5f023a1ac287ed
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-12-30 16:10:45 +0100

    Styling changes

--- components/general/UserBox/index.js
+++ components/general/UserBox/index.js
@@ -4,11 +4,9 @@ import css from "./index.module.css";
 
 export default function UserBox({ user, adminTag = false }) {
     return (
-        <div className={css.box}>
+        <Link href={"/users/" + user.id} className={css.box}>
             <Image height={50} width={50} src={user.avatar_url} alt="?" />
-            <Link href={"/users/" + user.id}>
-                {user.display_name} {adminTag && "(Admin)"}
-            </Link>
-        </div>
+            {user.display_name} {adminTag && "(Admin)"}
+        </Link>
     );
 }
