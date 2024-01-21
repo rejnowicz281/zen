47766af15fe0a70e6d53c2c92b6d12a2dda6a501
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-01-21 19:25:03 +0100

    Styling changes

--- components/general/Loading/index.js
+++ components/general/Loading/index.js
@@ -1,9 +1,10 @@
+import { VscLoading } from "react-icons/vsc";
 import css from "./index.module.css";
 
-export default function Loading() {
+export default function Loading({ spinnerSize = "50px" }) {
     return (
         <div className={css.wrapper}>
-            <div className={css.loading}>Loading...</div>
+            <VscLoading style={{ fontSize: spinnerSize }} className={css.spinner} />
         </div>
     );
 }
